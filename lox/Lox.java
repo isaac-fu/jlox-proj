bf2b22d7163d8e341165da031f202fe951401e69
Isaac Fu <isaacfu98@gmail.com>
2026-07-14 12:02:18 -0700

    progress1

--- lox/Lox.java
+++ lox/Lox.java
@@ -1,4 +1,3 @@
-package com.craftinginterpreters.lox;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,6 +6,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
+
+
 public class Lox {
     private static boolean hadError = false;
 
@@ -45,8 +46,7 @@ public class Lox {
         System.err.println(
         "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
-    }
- 
+    } 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
--- lox/Scanner.java
+++ lox/Scanner.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import static com.craftinginterpreters.lox.TokenType.*;
+
 class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
@@ -20,8 +18,38 @@ class Scanner {
             start = current;
             scanToken();
         }
-        tokens.add(new Token(EOF, "", null, line));
+        tokens.add(new Token(TokenType.EOF, "", null, line));
         return tokens;
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+        case '(': addToken(TokenType.LEFT_PAREN); break;
+        case ')': addToken(TokenType.RIGHT_PAREN); break;
+        case '{': addToken(TokenType.LEFT_BRACE); break;
+        case '}': addToken(TokenType.RIGHT_BRACE); break;
+        case ',': addToken(TokenType.COMMA); break;
+        case '.': addToken(TokenType.DOT); break;
+        case '-': addToken(TokenType.MINUS); break;
+        case '+': addToken(TokenType.PLUS); break;
+        case ';': addToken(TokenType.SEMICOLON); break;
+        case '*': addToken(TokenType.STAR); break;
+        }
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 
     private boolean isAtEnd() {
