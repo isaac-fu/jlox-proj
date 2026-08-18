69fe3f966ee4dfd4f5534beeb8fc45c9ae10dbdd
Isaac Fu <113050907+FuIsaac@users.noreply.github.com>
2026-08-18 11:50:02 -0700

    8/18 resolver

--- com/craftinginterpreters/lox/Environment.java
+++ com/craftinginterpreters/lox/Environment.java
@@ -27,6 +27,22 @@ class Environment {
         values.put(name, value);
     }
 
+    Environment ancestor(int distance) {
+        Environment environment = this;
+        for (int i = 0; i < distance; i++) {
+            environment = environment.enclosing;
+        }
+        return environment;
+    }
+
+    Object getAt(int distance, String name) {
+        return ancestor(distance).values.get(name);
+    }
+
+    void assignAt(int distance, Token name, Object value) {
+        ancestor(distance).values.put(name.lexeme, value);
+    }
+
     void assign(Token name, Object value) {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
@@ -34,7 +50,7 @@ class Environment {
         }
         if (enclosing != null) {
             enclosing.assign(name, value);
-            return
+            return;
         }
         throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
     }
--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -39,8 +39,12 @@ public class Lox {
         List<Token> tokens = scanner.scanTokens();
         Parser parser = new Parser(tokens);
         List<Stmt> statements = parser.parse();
-        // Stop if there was a syntax error.
         if (hadError) return;
+
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+        if (hadError) return;
+
         interpreter.interpret(statements);
     }
 
