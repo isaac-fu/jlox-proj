b01a86b7f4dbf777efec16f4212678e8e505c592
Isaac Fu <isaacfu98@gmail.com>
2026-08-24 21:26:48 -0700

    fix typo

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -68,8 +68,8 @@ class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
-            case "[": addToken(LEFT_BRACKET); break;
-            case "]": addToken(RIGHT_BRACKET); break;
+            case '[': addToken(LEFT_BRACKET); break;
+            case ']': addToken(RIGHT_BRACKET); break;
             case ':': addToken(COLON); break;
             case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
             case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
